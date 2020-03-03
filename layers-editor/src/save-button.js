36b3ca5613da0ddeda9ab227d8ff777662fd65da
michogar <michogar@gmail.com>
2020-03-03 10:53:18 +0100

    Modificada ruta de lectura del layer.json

--- layers-editor/src/save-button.js
+++ layers-editor/src/save-button.js
@@ -11,7 +11,7 @@ define([ 'jquery', 'message-bus', 'toolbar', 'i18n', './layers-api', 'ui/ui' ], 
 				contentType: 'application/json; charset=utf-8',
 				data: JSON.stringify(layerRoot.get(), null, 4),
 				success: function(data, textStatus, jqXHR) {
-					require([ 'text!../layers.json?a=' + new Date().getTime() ], function(newLayerRoot) {
+					require([ 'text!layers.json?a=' + new Date().getTime() ], function(newLayerRoot) {
 						bus.send('layers-set-root', JSON.parse(newLayerRoot));
 					});
 				},
