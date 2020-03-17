fc811ad6e30a64a0ffb57c372859a34cd6a98a9f
michogar <michogar@gmail.com>
2020-03-17 13:16:45 +0100

    Modificado comportamiento del botón de estadísticas, ahora muestra info de nuevo Juntados todos los CSSs en el layer.css

--- base/src/layer-stats-button.js
+++ base/src/layer-stats-button.js
@@ -1,21 +1,36 @@
-define([ 'message-bus', 'customization', 'ui/ui' ], function(bus, customization, ui) {
+define([ 'message-bus', 'i18n', 'customization', 'ui/ui' ], function(bus, i18n, customization, ui) {
+	var idStatsInfo = {
+		'link': 'static/loc/' + customization.languageCode + '/html/stats.html',
+		'title': i18n.stats_dialog_title
+	};
 
-	let buildButton = function(id) {
-		let link = ui.create('button', {
+	function getStatsLink(obj) {
+		return obj.hasOwnProperty('stats');
+	}
+
+	var buildLink = function(id) {
+		var link = ui.create('button', {
 			id: 'layer_stats_button_' + id,
-			css: 'layer_stats_button'
+			css: 'layer_stats_button layer_action_button',
+			clickEventCallback: function() {
+				bus.send('show-layer-stats');
+			}
 		});
 		return $(link);
 	};
 
-	bus.listen('before-adding-layers', () => {
-		let showInfoLayerAction = (portalLayer) => {
-			if (portalLayer.stats) {
-				return buildButton(portalLayer.id);
+	bus.listen('before-adding-layers', function() {
+		var showStatsLayerAction = function(portalLayer) {
+			if (getStatsLink(portalLayer)) {
+				return buildLink(portalLayer.id);
 			}
 			return null;
 		};
 
-		//bus.send('register-layer-action', showInfoLayerAction);
+		bus.send('register-layer-action', showStatsLayerAction);
+	});
+
+	bus.listen('show-layer-stats', function() {
+		bus.send('show-info', [ idStatsInfo.title, idStatsInfo.link ]);
 	});
 });
